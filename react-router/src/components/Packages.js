0f735d437d01dba6be07b506dfdd1d0e7064f140
Bachir <bachirmdicko@gmail.com>
2022-10-01 13:54:44 -0400

    Completed with no errors

--- react-router/src/components/Packages.js
+++ react-router/src/components/Packages.js
@@ -1,14 +1,14 @@
 
 
-export default function Packages() {
-
+export default function Packages(props) {
+    let packs = props.packages.map((pack) => <li>{pack}</li>)
     return (
         <div>
             <div className="packages">
                 <div className="packagesHeader">
                     <h1>Our Packages</h1>
                 </div>
-                
+                {packs}
             </div>
         </div>
     )
